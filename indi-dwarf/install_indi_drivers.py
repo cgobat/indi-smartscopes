4778e47b75ef1254dfde9ec094f2b82f6d8bdb18
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2026-03-18 14:16:56 -0700

    Add uninstall option to DWARF install script

--- indi-dwarf/install_indi_drivers.py
+++ indi-dwarf/install_indi_drivers.py
@@ -17,20 +17,34 @@ __version__ = config["dwarf"].get("version")
 
 
 def install() -> int:
+
+    while (proceed := input("\nYou are about to \033[4minstall\033[m the DWARF INDI driver."
+                            " Proceed? [Y/n] ").lower()) not in ("y", "yes", "n", "no"):
+        print(f"Unrecognized input '{proceed}'. Enter 'yes' or 'no'.")
+    if proceed.startswith("n"):
+        print("Aborting without action.\n")
+        return 0
+
     if not INDI_XML_DIR.is_dir():
-        print(f"Error: directory '{INDI_XML_DIR}' does not exist.")
+        print(f"Error: directory '{INDI_XML_DIR}' does not exist. Is the INDI library installed?")
         return 1
     xml_definition_file = INDI_XML_DIR / "drivers.xml"
+    if not xml_definition_file.is_file():
+        print(f"Error: XML file '{xml_definition_file}' does not exist. Is the INDI library installed?")
+        return 1
     driver_xml: xml._ElementTree = xml.parse(xml_definition_file.as_posix())
     dev_group: xml._Element = driver_xml.find("devGroup[@group='Telescopes']")
     for device_label in ["DWARF 3", "DWARF mini", "DWARF 2"]:
         existing = dev_group.find(f"device[@label='{device_label}']")
         if existing is None:
-            device_elem = xml.SubElement(dev_group, "device", {"label": device_label, "manufacturer": "DWARF Lab"})
+            device_elem = xml.SubElement(dev_group, "device", {"label": device_label, "manufacturer": "DWARFLAB"})
             driver_elem = xml.SubElement(device_elem, "driver", {"name": "pyINDI DWARF"})
             driver_elem.text = DRIVER_EXE_NAME
             version_elem = xml.SubElement(device_elem, "version")
             version_elem.text = __version__
+            print(f"- Added '{device_label}' driver definition to {xml_definition_file}")
+        else:
+            print(f"- Driver for '{device_label}' already exists in {xml_definition_file}")
     xml.indent(driver_xml, space=" "*4)
     driver_xml.write(xml_definition_file.as_posix(), encoding="UTF-8", pretty_print=True, xml_declaration=True)
 
@@ -40,12 +54,51 @@ def install() -> int:
     driver_destination = INDI_BIN_DIR / DRIVER_EXE_NAME
     driver_destination.unlink(missing_ok=True)
     driver_destination.symlink_to(driver_source)
-    print(f"Installed {driver_destination}")
+    print(f"- Installed driver executable: '{driver_destination}'")
+
+    print(f"\nNOTE: modifying or removing files in the source directory ({DRIVER_DIR}) may break this installation.\n")
     return 0
+
+def uninstall():
+
+    while (proceed := input("\nYou are about to \033[3;4mun\033[m\033[4minstall\033[m the DWARF"
+                            " INDI driver. Proceed? [Y/n] ").lower()) not in ("y", "yes", "n", "no"):
+        print(f"Unrecognized input '{proceed}'. Enter 'yes' or 'no'.")
+    if proceed.startswith("n"):
+        print("Aborting without action.\n")
+        return 0
+
+    xml_definition_file = INDI_XML_DIR / "drivers.xml"
+    try:
+        driver_xml: xml._ElementTree = xml.parse(xml_definition_file.as_posix())
+        dev_group: xml._Element = driver_xml.find(f"devGroup[@group='Telescopes']")
+        for device_label in ["DWARF 3", "DWARF mini", "DWARF 2"]:
+            existing = dev_group.find(f"device[@label='{device_label}']")
+            if existing is None:
+                print(f"- Driver for '{device_label}' is not currently present in {xml_definition_file}")
+            else:
+                dev_group.remove(existing)
+                print(f"- Removed '{device_label}' driver definition from {xml_definition_file}")
+        xml.indent(driver_xml, space=" "*4)
+        driver_xml.write(xml_definition_file.as_posix(), encoding="UTF-8",
+                         pretty_print=True, xml_declaration=True)
+    except OSError:
+        print(f"Warning: XML file '{xml_definition_file}' not found. No action taken.")
+
+    driver_path = INDI_BIN_DIR / DRIVER_EXE_NAME
+    try:
+        driver_path.unlink()
+        print(f"- Deleted '{driver_path}'")
+    except FileNotFoundError:
+        print(f"File '{driver_path} doesn't exist. No action taken.")
 
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
-        print(f'This script must be run with root privileges. Try `sudo {sys.argv[0]}`')
+        print(f"This script must be run with root privileges. Try `sudo {sys.argv[0]}`")
         sys.exit(1)
-    sys.exit(install())
+
+    if "--uninstall" in sys.argv:
+        sys.exit(uninstall())
+    else:
+        sys.exit(install())
